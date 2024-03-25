f3cf3bc6e54648f215ec3ed8e3e6f130fcfff6c0
fossil33 <chs4560522@naver.com>
2024-03-25 16:51:18 +0900

    test/#8 test

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import MobileFriendlyPage from './components/bso1';
+import { Announcement } from '@mui/icons-material';
+import AnotherComponent from './components/bso2';
+import ElevateAppBar from './components/Appbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <MobileFriendlyPage />
+    <AnotherComponent />
+    <ElevateAppBar />
+    <Footer />
+    </>
   );
 }
 
